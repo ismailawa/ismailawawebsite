f83937bc4bb0ad24d0618859b003bc1c75861ad4
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2021-06-02 11:48:08 +0100

    added server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+} )
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
