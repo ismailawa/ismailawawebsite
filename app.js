4d310f412376b41880af091991f1614147740fd8
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2021-06-02 12:21:44 +0100

    added server

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html');
 } )
 
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/public/about.html');
+} )
+
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
 })
